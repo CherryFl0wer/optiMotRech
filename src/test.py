419362ce32f13bfda8e526d3909385834f3f2704
Kelmatou <antoine.clop@epita.fr>
2018-06-30 15:34:00 +0200

    Setting up test.py

--- src/test.py
+++ src/test.py
@@ -1,22 +1,8 @@
-import collections
+from ngram import tf_idf_main
 
-ngram = 1
-line1 = ["I", "love", "that", "day", "its", "such", "a", "sunny", "day"]
-
-coll = collections.Counter()
-deck = collections.deque(maxlen=ngram)
-
-with open('./testing/alt.atheism/53068', 'rb', encoding='UTF8') as f:
-    for line in f:
-        for w in line:
-            if ngram == 1:
-                coll[w] += 1
-                continue
-
-            deck.append(w)
-            if len(deck) >= ngram:
-                ng = tuple(deck)
-                coll[ng] += 1
-
-print(deck)
-print(coll)
+# Usage:
+# $ python test.py
+if __name__ == '__main__':
+    test_folders = ["short_sentence", "short_text", "subset", "testing2", "wikipedia_test"]
+    for folder in test_folders:
+        tf_idf_main(["test.py", folder, folder + ".test.csv"])
